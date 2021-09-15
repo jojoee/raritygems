f928b860c5d390e5aa52493cff9f280938fae749
Nathachai Thongniran <inid3a@gmail.com>
2021-09-15 23:05:16 +0700

    chore(pypi): setup project for registry

---
+++ setup.py
@@ -0,0 +1,33 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+__version__ = "0.0.0"
+
+# This call to setup() does all the work
+setup(
+    name="raritygems",
+    version=__version__,
+    description="Provably Rare Gem: Raritygems",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jojoee/raritygems",
+    author="Nathachai Thongniran",
+    author_email="inid3a@gmail.com",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["raritygems"],
+    include_package_data=False,
+    install_requires=[
+
+    ],
+    entry_points={"console_scripts": ["raritygems=raritygems.__main__:main"]},
+)
