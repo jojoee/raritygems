b4b68f9220ffaa22954bf4880b588529cbd53a8d
Nathachai Thongniran <inid3a@gmail.com>
2021-09-15 23:04:11 +0700

    test: add test files

---
+++ tests/test_helper.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestTest:
+    def test_normal(self):
+        assert 3 > 2
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_raritygems.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestTest:
+    def test_normal(self):
+        assert 0 < 1
+
+
+if __name__ == "__main__":
+    unittest.main()
